1bdc98e6f37bc00509df2c3f2ba2d081ab1fb4f5
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-02 00:51:31 +0900

    ラジオボタンをクリックするとカテゴリ欄の有効無効が切り替わる記述

--- inputCheck.js
+++ inputCheck.js
@@ -1,1 +1,9 @@
-//第3章：UIの動作
+// ラジオボタンでカテゴリの有効無効を切り替える
+function disableSelectBox(disabled) {
+  document.getElementById("category").disabled = disabled;
+}
+
+// 収支入力フォームの内容をチェック
+function inputCheck() {
+
+}
