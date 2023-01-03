dc249a4e708f1e49e80d2d4cedf0f8df7d6f2357
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-04 01:35:48 +0900

    データ登録機能の実装

--- indexedDB.js
+++ indexedDB.js
@@ -13,7 +13,7 @@ database.onupgradeneeded = function (event) {
   console.log("データベースを新規作成しました");
 }
 
-// データベース接続に成功したときに発生するイベント
+// DB接続に成功したときに発生するイベント
 database.onsuccess = function (event) {
   let db = event.target.result;
   // 接続を解除する（DB操作が終了したら必ずcloseする）
@@ -23,3 +23,79 @@ database.onsuccess = function (event) {
 database.onerror = function (event) {
   console.log("データベースに接続できませんでした");
 }
+
+// フォームの内容をDBに登録する
+function regist() {
+  // フォームの入力チェック。falseが返ったら登録処理を中断
+  if (inputCheck() == false) {
+    return;
+  }
+
+  // ラジオボタンの取得
+  let radio = document.getElementsByName("balance");
+  let balance;
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked == true) {
+      balance = radio[i].ariaValueMax;
+      break;
+    }
+  }
+
+  // フォームの値を取得
+  let date = document.getElementById("date").value;
+  let category = document.getElementById("category").value;
+  let amount = document.getElementById("amount").value;
+  let memo = document.getElementById("memo").value;
+  if (balance == "収入") {
+    category = "収入";
+  }
+
+  // DBにデータを登録する
+  insertData(balance, date, category, amount, memo);
+
+  // データの挿入
+  function insertData(balance, date, category, amount, memo) {
+    // 一意のIDを現在の日時から作成
+    let uniqueID = new Date().getTime().toString();
+    console.log(uniqueID);
+    // DBに登録するための連想配列のデータを作成
+    let data = {
+      id: uniqueID,
+      balance: balance,
+      date: String(date),
+      category: category,
+      amount: amount,
+      memo: memo,
+    }
+
+    // DBを開く
+    let database = indexedDB.open(dbName, dbVersion);
+    // DBの開けなかったときの処理
+    database.onerror = function (event) {
+      console.log("データベースに接続できませんでした");
+    }
+    // DBが開いたらデータの登録を実行
+    database.onsuccess = function (event) {
+      let db = event.target.result;
+      let transaction = db.transaction(storeName, "readwrite");
+      transaction.oncomplete = function (event) {
+        console.log("トランザクション完了");
+      }
+      transaction.onerror = function (event) {
+        console.log("トランザクションエラー");
+      }
+
+      let store = transaction.objectStore(storeName);
+      let addData = store.add(data);
+      addData.onsuccess = function () {
+        console.log("データが登録できました");
+        alert("登録しました");
+      }
+      addData.onerror = function () {
+        console.log("データが登録できませんでした");
+      }
+      db.close();
+    }
+  }
+}
+
